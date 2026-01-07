440833e8fc0f8598589c0ec2dce9a6406311d94a
aayushsingh123sh-sys <aayushsingh123sh@gmail.com>
2026-01-07 19:38:19 +0530

    Update visualize_results.py

--- scripts/visualize_results.py
+++ scripts/visualize_results.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import plotly.express as px
 
-# 1. Load the processed data
 df = pd.read_csv('data/logistics_data_processed.csv')
 
-# 2. Create a Summary: Average Cost vs Average CO2 per Carrier
 summary = df.groupby('Carrier')[['Cost_USD', 'CO2_kg']].mean().reset_index()
 
-# 3. Build an Interactive Scatter Plot
 fig = px.scatter(
     summary, 
     x="Cost_USD", 
@@ -21,8 +18,6 @@ fig = px.scatter(
 
 fig.update_traces(textposition='top center')
 
-# 4. Save the chart as an Image or HTML file
-# This allows you to put the chart in your GitHub README
 fig.write_image("logistics_chart.png") 
 fig.show()
 
